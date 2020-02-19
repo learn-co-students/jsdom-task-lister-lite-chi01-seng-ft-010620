e8ac6f17def86373a266a93b054e29f3ef7e2134
Zach Weber <weberzt@gmail.com>
2020-02-19 13:56:20 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskForm = document.getElementById("create-task-form");
   taskForm.addEventListener('submit', function(event) {
     event.preventDefault();
+
     let tasks = document.getElementById('tasks');
     let userInput = event.target.children[1].value;
     let createNewNode = document.createElement("li");
     createNewNode.innerText = userInput;
     tasks.appendChild(createNewNode);
-    taskForm.reset();
+      taskForm.reset();
+
+    let button = document.createElement("BUTTON");
+    button.innerHTML = "X";
+    createNewNode.appendChild(button);
+    button.addEventListener("click", function () {
+      createNewNode.remove();
+    })
   })
 });
+
+// let button = document.createElement("BUTTON");
+// let buttonChild = createNewNode.appendChild(button);
+// buttonChild.innerHTML = "X";
+// createNewNode.remove()
