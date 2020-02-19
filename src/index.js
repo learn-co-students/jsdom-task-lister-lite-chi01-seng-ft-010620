c4c5a5bfb8053b6602811ecfbf4e67c7ab318ce7
Niko Dixon <nikodixon@gmail.com>
2020-02-19 11:48:16 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelectorAll("input")[1].addEventListener("click", function(event) {
+    let ul = document.getElementById("tasks")
+    let task = document.createElement('li')
+    ul.appendChild(task)
+    let input = document.getElementById("new-task-description")
+    task.innerHTML = input.value
+    event.preventDefault()
+  })
 });
+
