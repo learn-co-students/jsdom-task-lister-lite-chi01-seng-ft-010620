ff3321029245cb88bab1a607a806f52b26803703
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-02-25 21:54:13 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
-const taskForm = document.getElementById("create-task-form");
-const taskList = document.getElementById("tasks");
+document.addEventListener("DOMContentLoaded", () => {
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
 
-// const main = document.getElementById("main-content")
-// const sortTaskBtn = document.
+    let tasks = document.getElementById("tasks");
+    let userInput = event.target.children[1].value;
+    let li = document.createElement("li");
+    li.innerText = userInput;
+    tasks.appendChild(li);
+    taskForm.reset();
 
-function handleSubmitTask(event) {
-  event.preventDefault();
-  const userInput = event.target.children[1].value;
-  const li = document.createElement("li");
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "X";
-  deleteBtn.style.float = "right";
-  li.innerText = userInput;
-  li.appendChild(deleteBtn);
-  taskList.appendChild(li);
-  event.target.reset();
-}
-
-function handleDeleteTask(event) {
-  if (event.target.tagName === "BUTTON") {
-    event.target.parentElement.remove();
-  }
-}
-
-// function handleSortedTasks(event) {
-// cont taskArray = Array
-
-// }
-
-taskForm.addEventListener("submit", handleSubmitTask);
-taskList.addEventListener("click", handleDeleteTask);
-// sortTaskBtn.addEventListener("click", handleSortedTasks);
+    let button = document.createElement("button");
+    button.innerHTML = "X";
+    button.style.float = "right";
+    li.appendChild(button);
+    button.addEventListener("click", function() {
+      li.remove();
+    });
+  });
+});
